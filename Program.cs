fac94197498d666d7b842e01b4ad1e736b8611a2
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-31 19:03:12 +0530

    Added:UC5 program file, checking attendance and calculating monthly wage

--- Program.cs
+++ Program.cs
@@ -8,31 +8,61 @@ namespace Employee_Wage
         {
             Console.WriteLine(" **Employee Wage Computation** \n");
             Random obj = new Random();
-
+            int Day = 0;
+            int  totalday = 0;
+            int fullDayPresent = 0;
+            int halfDayPresent = 0;
+            int absent = 0;
             int dailyWadge = 0;
             int fullDayhour = 8;
             int halfDayhour = 4;
             int perHourWage = 20;
+            int totalWage = 0;
 
-            int check = obj.Next(0,3);
+         while(Day!=20 && totalday!=30)
 
-            switch (check ) 
             {
-                case 0 :
-                         Console.WriteLine("Employee is Absent ");
-                    break;
+                int check = obj.Next(0, 3);
 
-                case 1 :
-                          dailyWadge = perHourWage * halfDayhour;
-                          Console.WriteLine("Employee is Present for half day his Daily wage  "+dailyWadge );
-                    break;
+                switch (check)
+                {
+                    case 0:
+                        totalday++;
+                        absent++;
+                        break;
 
-                case 2:
-                          dailyWadge = perHourWage * fullDayhour;
-                          Console.WriteLine("Employee is Present for full day his Daily wage  " + dailyWadge);
-                    break;
+                    case 1:
+                        dailyWadge = perHourWage * halfDayhour;
+                        totalWage += dailyWadge;
+                        halfDayPresent++;
+                        Day++;
+                        totalday++;
+                        break;
+
+                    case 2:
+                        dailyWadge = perHourWage * fullDayhour;
+                        totalWage += dailyWadge;
+
+                        fullDayPresent++;
+                        Day++;
+                        totalday++;
+                        break;
+                }
+
+
             }
 
+            Console.WriteLine("Employee absent in a month - "+absent );
+            Console.WriteLine("Employee half day present in 0a month  - " + halfDayPresent);
+            Console.WriteLine("Employee full day present in a month  - " + fullDayPresent);
+            Console.WriteLine("Employee total wage  - " + totalWage);
+            Console.WriteLine("Employee present in a month  - " + (halfDayPresent+fullDayPresent));
+
+
+
+
         }
+
+
     }
 }
