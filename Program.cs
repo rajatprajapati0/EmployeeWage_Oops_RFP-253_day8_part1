e3b5ad0eb9ef6c8300f39e57fa06bf9664eaf5b9
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-31 23:23:41 +0530

    Refactor the Code to write a Class Method Use Class Method and Class Variables

--- Program.cs
+++ Program.cs
@@ -1,64 +1,31 @@
 ﻿using System;
 
+
 namespace Employee_Wage
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" **Employee Wage Computation** \n");
-            Random obj = new Random();
-            int Day = 20;
-            int  totalHour= 0;
-            int fullDayPresent = 0;
-            int halfDayPresent = 0;
-            int absent = 0;
-            int dailyWadge = 0;
-            int fullDayhour = 8;
-            int halfDayhour = 4;
-            int perHourWage = 20;
-            int totalWage = 0;
 
-         for (int i=0; i<Day && totalHour<=100; i++ )
+            logic obj = new logic();
+            int totalWage=obj.EmployeeWage();
+            int absent = obj.absent;
+           int totalpresent=obj.totalpresent;
+            int totalHour = obj.totalHour;
+            int halfDayPresent=obj.halfDayPresent;
 
-         {
-                int check = obj.Next(0, 3);
+            int fullDayPresent = obj.fullDayPresent;
 
-               
-                
-                    switch (check)
-                    {
-                        case 0:
-
-                            absent++;
-                            break;
-
-                        case 1:
-                            dailyWadge = perHourWage * halfDayhour;
-                            totalWage += dailyWadge;
-                            halfDayPresent++;
-                            totalHour = totalHour + halfDayhour;
-                            break;
-
-                        case 2:
-                            dailyWadge = perHourWage * fullDayhour;
-                            totalWage += dailyWadge;
-                            totalHour = totalHour + fullDayhour;
-                            fullDayPresent++;
-
-                            break;
-                    }
-         }
 
             Console.WriteLine("Employee absent in a month - "+absent );
             Console.WriteLine("Employee half day present in a month  - " + halfDayPresent);
             Console.WriteLine("Employee full day present in a month  - " + fullDayPresent);
             Console.WriteLine("Employee total wage  - " + totalWage);
-            Console.WriteLine("Employee present in a month  - " + (halfDayPresent+fullDayPresent));
+            Console.WriteLine("Employee present day in a month  - " + totalpresent);
             Console.WriteLine("Employe worked hour in a month - " + totalHour);
             Console.ReadLine();
-
-
+            
         }
        
 
---
+++ logic.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage
+{
+    internal class logic
+    {
+     
+        static int Day = 20;
+        public int totalHour = 0;
+        public int fullDayPresent = 0;
+        public int halfDayPresent = 0;
+        public int absent = 0;
+        static int dailyWadge = 0;
+        static int fullDayhour = 8;
+        static int halfDayhour = 4;
+        static int perHourWage = 20;
+        public int totalpresent = 0;
+
+        
+
+        public int EmployeeWage()
+
+        {
+            Console.WriteLine(" **Employee Wage Computation** \n");
+            Random obj = new Random();
+            
+            int totalWage = 0;
+
+            for (int i = 0; i < Day && totalHour <= 100; i++)
+
+            {
+                int check = obj.Next(0, 3);
+
+
+
+                switch (check)
+                {
+                    case 0:
+
+                        absent++;
+                        break;
+
+                    case 1:
+                        dailyWadge = perHourWage * halfDayhour;
+                        totalWage += dailyWadge;
+                        halfDayPresent++;
+                        totalHour = totalHour + halfDayhour;
+                        break;
+
+                    case 2:
+                        dailyWadge = perHourWage * fullDayhour;
+                        totalWage += dailyWadge;
+                        totalHour = totalHour + fullDayhour;
+                        fullDayPresent++;
+
+                        break;
+
+                        
+                }
+
+                totalpresent = fullDayPresent + halfDayPresent;
+                
+            }
+
+            return totalWage ;
+           
+        }
+
+    }
+}
