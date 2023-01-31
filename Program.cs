0808e91bc5cebe6ba1e6223a335287fed9a2939f
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-31 15:20:58 +0530

    Added :UC1 program file Employee is Absen or present by Random

--- Program.cs
+++ Program.cs
@@ -7,7 +7,21 @@ namespace Employee_Wage
         static void Main(string[] args)
         {
             Console.WriteLine(" **Employee Wage Computation**");
+
+            Random obj = new Random();
+            int check = obj.Next(0,2);
+
+            if (check==0 ) 
+            {
+                Console.WriteLine("Employee is Absent ");
             
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present ");
+
+            }
+
         }
     }
 }
